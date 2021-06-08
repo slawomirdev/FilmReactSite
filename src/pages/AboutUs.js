4def81e93d547f3114a9d2d2af1ee48e3c4ae6c2
thewilkupl <thewilku@o2.pl>
2021-06-08 23:30:09 +0200

    2. About Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//Page components
+import AboutSections from '../components/AboutSection'
+
+const AboutUs = () => {
+  return <div>
+      <AboutSections/>
+  </div>
+}
+
+export default AboutUs
