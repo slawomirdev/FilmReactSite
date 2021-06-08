a9f4bad5a8b67fecd6a0f91c90107108eabb131c
thewilkupl <thewilku@o2.pl>
2021-06-08 23:45:30 +0200

    4. FAQ section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from 'react'
 //Page components
 import AboutSections from '../components/AboutSection'
 import ServicesSection from '../components/ServicesSection'
+import FaqSection from '../components/FaqSection'
 
 const AboutUs = () => {
   return (
     <>
       <AboutSections />
       <ServicesSection />
+      <FaqSection />
     </>
   )
 }
