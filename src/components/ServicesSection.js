7c692bddb6ffaa8e299e0bb16d9e4b16349cb168
thewilkupl <thewilku@o2.pl>
2021-06-08 23:41:15 +0200

    3. Services section

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,54 @@
+import React from 'react'
+//import icons
+import clock from '../img/clock.svg'
+import diaphrahm from '../img/diaphragm.svg'
+import money from '../img/money.svg'
+import teamwork from '../img/teamwork.svg'
+import home2 from '../img/home2.png'
+
+const ServicesSection = () => {
+  return (
+    <div className="services">
+      <div className="description">
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={clock} alt="clock" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={money} alt="money" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={teamwork} alt="teamwork" />
+              <h3>TeamWork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={diaphrahm} alt="diaphrahm" />
+              <h3>Diaphrahm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div className="image">
+        <img src={home2} alt="work" />
+      </div>
+    </div>
+  )
+}
+
+export default ServicesSection
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,15 @@
 import React from 'react'
 //Page components
 import AboutSections from '../components/AboutSection'
+import ServicesSection from '../components/ServicesSection'
 
 const AboutUs = () => {
-  return <div>
-      <AboutSections/>
-  </div>
+  return (
+    <>
+      <AboutSections />
+      <ServicesSection />
+    </>
+  )
 }
 
 export default AboutUs
