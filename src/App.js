54f8137634633ca5e63046d2629a7ca50d4810ec
thewilkupl <thewilku@o2.pl>
2021-06-09 17:44:52 +0200

    5. Styling AboutSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import GlobalStyle from './components/GlobalStyle'
+//global style
+
 //import pages
 import AboutUs from './pages/AboutUs'
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AboutUs />
     </div>
   )
