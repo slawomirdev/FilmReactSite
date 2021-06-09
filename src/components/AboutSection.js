908cdfb64a5cba65f8c06179de630cd09d7a3250
thewilkupl <thewilku@o2.pl>
2021-06-09 22:42:54 +0200

    13. add animated svg

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,6 +7,7 @@ import { About, Description, Image, Hide } from '../styles'
 //framer motion
 import { motion } from 'framer-motion'
 import { titleAnim, fade, photoAnim } from '../animation'
+import Wave from './Wave'
 
 const AboutSection = () => {
   return (
@@ -34,6 +35,7 @@ const AboutSection = () => {
       <Image>
         <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
+      <Wave />
     </About>
   )
 }
