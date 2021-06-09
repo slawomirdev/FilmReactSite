b8a1069447ce2322b821d1da01cd18fa2a65c5a5
thewilkupl <thewilku@o2.pl>
2021-06-09 19:51:41 +0200

    9. Add Routing and Style 2nd page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import GlobalStyle from './components/GlobalStyle'
 
 //import pages
 import AboutUs from './pages/AboutUs'
+import ContactUs from './pages/ContactUs'
+import OurWork from './pages/OurWork'
 import Nav from './components/Nav'
+
+//router
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   )
 }
